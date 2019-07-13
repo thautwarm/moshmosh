0adfea77ecc7e5dd29a668313a9c2cfb673deaf0
thautwarm <twshere@outlook.com>
2019-07-13 15:22:59 +0800

    tests got passed at test.pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from datetime import datetime
 
-with Path('README.md').open() as readme:
+with open('README.md') as readme:
     readme = readme.read()
 
 version = "0.1.0"
