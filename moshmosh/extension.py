4d7849c97bf12f00959eff98a86a9d90ba168802
thautwarm <twshere@outlook.com>
2019-10-07 22:14:01 +0800

    rewrite system

--- moshmosh/extension.py
+++ moshmosh/extension.py
@@ -1,24 +1,48 @@
-from rbnf.py_tools.unparse import Unparser
-from io import StringIO
 import ast
+from io import StringIO
+from moshmosh.rewrite_helper import ast_to_literal
 
 
 class PIP(ast.NodeTransformer):
+    """
+    demo transformer
+    """
     def visit_Name(self, name: ast.Name):
         if name.id == 'PIP':
-            return ast.Constant(114514)
+            return ast.Constant(114514, lineno=name.lineno, col_offset=name.col_offset)
         return name
-
-
-def unparse(node: ast.AST):
-    io = StringIO()
-    Unparser(node, file=io)
-    return io.getvalue()
 
 
 def extension(source_code):
     # FIXME: Haskell-like syntax extension system
     node = ast.parse(source_code)
     node = PIP().visit(node)
-    source_code = unparse(node)
-    return "print('114514')\n" + source_code
+    literal = ast_to_literal(node)
+    string_io = StringIO()
+    string_io.write("""
+import ast as _ast
+def _literal_to_ast(literal):
+    '''
+    Convert a python literal to an AST.
+    '''
+    if isinstance(literal, dict):
+        ctor = literal.pop('constructor')
+        ctor = getattr(_ast, ctor)
+        return ctor(**{k: _literal_to_ast(v) for k, v in literal.items()})
+
+    if isinstance(literal, list):
+        return list(map(_literal_to_ast, literal))
+
+    return literal
+
+    """)
+    string_io.write('\n')
+    string_io.write('__literal__ = ')
+    string_io.write(repr(literal))
+    string_io.write('\n')
+    string_io.write('__ast__ = _literal_to_ast(__literal__)\n')
+    # string_io.write('from astpretty import pprint;pprint(__ast__)\n')
+    string_io.write('__code__ = compile(__ast__, __file__, "exec")\n')
+    string_io.write('exec(__code__, globals())\n')
+    code = string_io.getvalue()
+    return code
---
+++ moshmosh/rewrite_helper.py
@@ -0,0 +1,43 @@
+import ast
+
+
+def ast_to_literal(node: ast.AST):
+    """
+    Convert an AST to a python literal.
+    We avoid the use of comprehension expressions here
+    to get more human-friendly call stacks.
+    """
+    if isinstance(node, ast.AST):
+        field_names = node._fields
+        res = {'constructor': node.__class__.__name__}
+        for field_name in field_names:
+            field = getattr(node, field_name)
+            field = ast_to_literal(field)
+            res[field_name] = field
+        if hasattr(node, 'lineno'):
+            res['lineno'] = node.lineno
+        if hasattr(node, 'col_offset'):
+            res['col_offset'] = node.col_offset
+
+        return res
+    if isinstance(node, list):
+        res = []
+        for each in node:
+            res.append(ast_to_literal(each))
+        return res
+    return node
+
+
+def literal_to_ast(literal):
+    """
+    Convert a python literal to an AST.
+    """
+    if isinstance(literal, dict):
+        ctor = literal.pop('constructor')
+        ctor = getattr(ast, ctor)
+        return ctor(**{k: literal_to_ast(v) for k, v in literal.items()})
+
+    if isinstance(literal, list):
+        return list(map(literal_to_ast, literal))
+
+    return literal
