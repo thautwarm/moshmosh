47c4996acaf8620e9b33f23ea59bcdc7316ff54b
thautwarm <twshere@outlook.com>
2019-12-19 22:17:43 +0900

    disable warnings for purposeful ops

--- moshmosh/__init__.py
+++ moshmosh/__init__.py
@@ -2,6 +2,7 @@ from .ast_compat import ast
 from .extension_register import *
 from .extensions import template_python
 from .extensions import lazy_import
+import warnings
 with warnings.catch_warnings():
     warnings.simplefilter("ignore", category=SyntaxWarning)
     from .extensions import pattern_matching
